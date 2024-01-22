3e4f1338e6fb0dbb905293a556e79d3fff3b1a17
sofffja <sofi.efron@gmail.com>
2024-01-22 15:59:28 -0300

    Add simple game with create players and play function

--- script.js
+++ script.js
@@ -19,6 +19,19 @@ const gameboard = (function() {
 
 })();
 
+const game = (function() {
+  const playerOne = createPlayer('A', prompt('choose x or o'))
+  const playerTwo = createPlayer('B', playerOne.getSymbol() === 'x' ? 'o' : 'x');
+
+  const play = () => {
+    playerOne.selectMove(prompt(`player one, choose row`) - 1, prompt(`player one, choose column`) - 1);
+    playerTwo.selectMove(prompt(`player two, choose row`) - 1, prompt(`player two, choose column`) - 1);
+  }
+
+  return { play }
+
+})();
+
 function createPlayer(name, symbol) {
   const selectMove = function(row, col) {
     gameboard.setArr(symbol, row, col)
