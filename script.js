2d65274bfe0eeb02cfd6abf6650411e835f35435
sofffja <sofi.efron@gmail.com>
2024-01-23 08:27:57 -0300

    Add basic winner function

--- script.js
+++ script.js
@@ -1,40 +1,97 @@
 const gameboard = (function() {
-  let arr = [
+  let board = [
     ['','',''],
     ['','',''],
     ['','','']
   ];
 
-  const setArr = (symbol, row, col) => {
-    if (!arr[row][col]) {
-      arr[row][col] = symbol;
+  const setBoard = (symbol, row, col) => {
+    if (!board[row][col]) {
+      board[row][col] = symbol;
     } else  {
       console.log(`cell taken`)
     }
   }
 
-  const getArr = () => arr
+  const getBoard = () => board;
 
-  return { getArr, setArr };
+  const isFull = () => {
+    for (const row of board) {
+      for (const column of row) {
+        if (column == false) return false;
+      }
+    }
+    return true;
+  }
+
+  const resetBoard = () => {
+    board = [
+      ['','',''],
+      ['','',''],
+      ['','','']
+    ];
+  }
+
+  const getWinCases = () => {
+    return [
+      isWinCase(board[0][0], board[0][1], board[0][2]),
+      isWinCase(board[1][0], board[1][1], board[1][2]),
+      isWinCase(board[2][0], board[2][1], board[2][2]),
+  
+      isWinCase(board[0][0], board[1][0], board[2][0]),
+      isWinCase(board[0][1], board[1][1], board[2][1]),
+      isWinCase(board[0][2], board[1][2], board[2][2]),
+  
+      isWinCase(board[0][0], board[1][1], board[2][2]),
+      isWinCase(board[0][2], board[1][1], board[2][0])
+    ]
+  } 
+
+  return { getBoard, setBoard, isFull, getWinCases, resetBoard };
 
 })();
 
 const game = (function() {
-  const playerOne = createPlayer('A', prompt('choose x or o'))
+  const playerOne = createPlayer('A', 'x')
   const playerTwo = createPlayer('B', playerOne.getSymbol() === 'x' ? 'o' : 'x');
 
   const play = () => {
-    playerOne.selectMove(prompt(`player one, choose row`) - 1, prompt(`player one, choose column`) - 1);
-    playerTwo.selectMove(prompt(`player two, choose row`) - 1, prompt(`player two, choose column`) - 1);
+    for (let i = 1; i <= 9; i++) {
+      if (!checkWinner()) {
+        if (i % 2 !== 0) {
+          playerOne.selectMove(prompt(`player one, choose row`), prompt(`player one, choose column`));
+          console.table(gameboard.getBoard())
+        } else {
+          playerTwo.selectMove(prompt(`player two, choose row`), prompt(`player two, choose column`));
+          console.table(gameboard.getBoard())
+        }
+      } else if (checkWinner() === 'tie') {
+        return 'tie! end game'
+      } else {
+        return checkWinner() === playerOne.getSymbol() ?
+          `${playerOne.getName()} wins` :
+          `${playerTwo.getName()} wins`
+      }
+    }
   }
 
-  return { play }
+  const checkWinner = () => {
+    for (const winCase of gameboard.getWinCases()) {
+      if (winCase.symbol && winCase.win) {
+        return winCase.symbol;
+      } else if (gameboard.isFull()) {
+        return 'tie';
+      }
+    }
+  }
+
+  return { play, checkWinner }
 
 })();
 
 function createPlayer(name, symbol) {
   const selectMove = function(row, col) {
-    gameboard.setArr(symbol, row, col)
+    gameboard.setBoard(symbol, row, col)
   }
 
   const getName = () => name;
@@ -43,3 +100,11 @@ function createPlayer(name, symbol) {
   return { getName, getSymbol, selectMove }
 }
 
+function isWinCase(){
+  var len = arguments.length;
+  for (var i = 1; i< len; i++){
+     if (arguments[i] === null || arguments[i] !== arguments[i-1])
+        return false;
+  }
+  return { win: true, symbol: arguments[0] };
+}
